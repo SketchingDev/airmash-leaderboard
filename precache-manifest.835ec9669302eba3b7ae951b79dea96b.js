c511f58411cbfac49849603aef87fab325d89a24
SketchingDev <31957045+SketchingDev@users.noreply.github.com>
2020-07-15 22:06:42 +0100

    Updates

---
+++ precache-manifest.835ec9669302eba3b7ae951b79dea96b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a3f7454f2fbf41a70b414dddbf103a8",
+    "url": "/airmash-leaderboard/index.html"
+  },
+  {
+    "revision": "dfb52a3d549babe88ad4",
+    "url": "/airmash-leaderboard/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "80e22604a08c3855f5eb",
+    "url": "/airmash-leaderboard/static/js/2.6864048d.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/airmash-leaderboard/static/js/2.6864048d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfb52a3d549babe88ad4",
+    "url": "/airmash-leaderboard/static/js/main.5f4d29ac.chunk.js"
+  },
+  {
+    "revision": "768d3be089c70d095b63",
+    "url": "/airmash-leaderboard/static/js/runtime-main.7c85df62.js"
+  }
+]);
