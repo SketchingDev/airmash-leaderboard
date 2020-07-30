c2c806d05342efe64259e3b791dfea556d5a1772
SketchingDev <31957045+SketchingDev@users.noreply.github.com>
2020-07-30 23:37:02 +0100

    Add website env generater

---
+++ packages/website/generate-env.js
@@ -0,0 +1,21 @@
+const AWS = require("aws-sdk");
+
+const region = "us-east-1";
+const stackName = "observer-bot-dev";
+
+const cloudFormation = new AWS.CloudFormation({ region, apiVersion: "2010-05-15" });
+
+cloudFormation.describeStacks(
+  {
+    StackName: stackName,
+  },
+  (err, data) => {
+    data.Stacks.forEach(stack => {
+      stack.Outputs.forEach(output => {
+        if (output.OutputKey === "ServiceEndpoint") {
+          console.log(`REACT_APP_API_ENDPOINT=${output.OutputValue}`);
+        }
+      });
+    });
+  },
+);
